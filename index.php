3ad17fea52440dda37c4536266f512c8a3224054
juancarlosmtz3 <jcarlosmtz3@gmail.com>
2018-05-12 17:32:28 -0500

    se guardan los cambios de la solicitud a openweather

--- index.php
+++ index.php
@@ -5,13 +5,23 @@ require 'vendor/autoload.php';
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-
-$app = new \Slim\App;
+$configuration = [
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+];
+$c = new \Slim\Container($configuration);
+$app = new \Slim\App($c);
 
 $app->get('/clima', function (Request $request, Response $response, array $args) {
-	
-    $data = array('name' => 'Juan Carlos', 'count' => 414118186);
+    
+    $client = new \GuzzleHttp\Client();
+    $res = $client->request('GET', 'http://api.openweathermap.org/data/2.5/weather?id=3529612&appid=9a8b7903ee8ea59c827c7ff32b9dfce5&units=metric');
+ 
+    $body =  $res->getBody()->getContents();
 
-    return $response->withJson($data);
+    $json = json_decode($body,true);
+
+    return $response->withJson($json);
 });
 $app->run();
